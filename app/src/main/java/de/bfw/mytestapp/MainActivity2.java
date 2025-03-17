d28717954fdc64ef6f6d4e94a8bfad963f3a8fae
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-03-17 08:48:44 +0100

    Add switchBtn with functionality also to MainActivity2

--- app/src/main/java/de/bfw/mytestapp/MainActivity2.java
+++ app/src/main/java/de/bfw/mytestapp/MainActivity2.java
@@ -1,24 +1,42 @@
 package de.bfw.mytestapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener{
+
+    AppCompatButton switchB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+        /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+         */
+        switchB = findViewById(R.id.switchB);
+
+        switchB.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == switchB.getId()) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
